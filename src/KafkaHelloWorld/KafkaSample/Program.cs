31b30d6f19b90857e1ecde772dc673e978247db4
a.afsharinejad <a.afsharinejad@mohaymen.ir>
2023-10-04 15:12:25 +0330

    read

--- src/KafkaHelloWorld/KafkaSample/Program.cs
+++ src/KafkaHelloWorld/KafkaSample/Program.cs
@@ -1,37 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Xml;
 using Confluent.Kafka;
 
 Console.WriteLine($"Starting {DateTime.Now}");
 
-var config = new ProducerConfig
-{
-    BootstrapServers = "localhost:9092",
-};
-
-using var producer = new ProducerBuilder<string, string>(config).Build();
-
-var tasks = new List<Task>
-{
-    Produce(producer, 0),
-    Produce(producer, 1),
-    Produce(producer, 2),
-    Produce(producer, 3),
-    Produce(producer, 4)
-};
-
-await Task.WhenAll(tasks);
+//await Write();
+await Read();
 
 Console.WriteLine($"End {DateTime.Now}");
 Console.ReadLine();
 
-async Task Produce(IProducer<string, string> kafkaProducer, int index)
+async Task Write()
+{
+    var config = new ProducerConfig
+    {
+        BootstrapServers = "localhost:9092"
+    };
+
+    using var producer = new ProducerBuilder<string, string>(config).Build();
+
+    var tasks = new List<Task>
+    {
+        Produce(producer, 0),
+        Produce(producer, 1),
+        Produce(producer, 2),
+        Produce(producer, 3),
+        Produce(producer, 4)
+    };
+
+    await Task.WhenAll(tasks);
+}
+
+async Task Produce(IProducer<string, string> producer, int index)
 {
     const int max = 100000;
     var start = index * max;
     for (var i = start; i < start + max; i++)
     {
-        await kafkaProducer.ProduceAsync("second-topic2", 
+        await producer.ProduceAsync("second-topic2",
             new Message<string, string> { Key = $"k{i}", Value = $"v{i}" });
     }
+}
+
+async Task Read()
+{
+    var config = new ConsumerConfig
+    {
+        BootstrapServers = "localhost:9092",
+        GroupId = "g1",
+        EnableAutoCommit = false,
+        AutoOffsetReset = AutoOffsetReset.Earliest,
+        EnablePartitionEof = true
+    };
+
+    using var consumer = new ConsumerBuilder<string, string>(config).Build();
+    consumer.Subscribe(new[] { "second-topic1" });
+
+    int count = 0;
+    while (true)
+    {
+        var result = consumer.Consume();
+        count++;
+        //Console.WriteLine(result?.Message?.Value);
+        if (result?.IsPartitionEOF ?? true) break;
+    }
+
+    Console.WriteLine($"Total: {count}");
 }
