35992d03a640d930b020ea96d1e73cc9ae458276
a.afsharinejad <a.afsharinejad@mohaymen.ir>
2023-10-18 09:01:42 +0330

    sql

--- src/StreamProcessing/Odbc.Sample/Program.cs
+++ src/StreamProcessing/Odbc.Sample/Program.cs
@@ -1,17 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Collections.Generic;
 using System.Data.Odbc;
+using System.Threading.Tasks;
 
 #pragma warning disable CS8321
 
-const string ConnectionString = @"Driver={ClickHouse ODBC Driver (Unicode)};Host=localhost;PORT=8123;Timeout=500;Username=admin;Password=admin";
+//const string ConnectionString = @"Driver={ClickHouse ODBC Driver (Unicode)};Host=localhost;PORT=8123;Timeout=500;Username=admin;Password=admin";
+string ConnectionString = "Driver={SQL Server};Server=192.168.30.171;Uid=sa;Pwd=!QAZ2wsx;";
+
 
 Console.WriteLine($"Start {DateTime.Now}");
 
 //InsertRow();
 //InsertRowWithParameter();
-ReadRow();
+
+var list = new List<Task>();
+for (int i = 0; i < 100; i++)
+{
+    list.Add(Task.Run(ReadRow));
+}
+
+await Task.WhenAll(list);
 
 Console.WriteLine($"Finish {DateTime.Now}");
 
@@ -49,13 +60,28 @@ void ExecuteNonQuery(OdbcCommand command)
 
 void ReadRow()
 {
-    const string query = "select id, name from test";
+    const string query = "SELECT 1 as dateTime";
     using var connection = new OdbcConnection(ConnectionString);
     using var command = new OdbcCommand(query, connection);
     connection.Open();
-    var reader = command.ExecuteReader();
-    while (reader.Read())
+    for (int i = 0; i < 5000; i++)
     {
-        Console.WriteLine(reader["id"]);
+        try
+        {
+            using var reader = command.ExecuteReader();
+            //Console.WriteLine("haha");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($" Error Time: {DateTime.Now}\n{e.Message}");
+        }
+        
+        // while (reader.Read())
+        // {
+        //     if (i % 500 == 0)
+        //         Console.WriteLine(reader["dateTime"]);
+        // }
     }
+
+    Console.WriteLine("finish");
 }
